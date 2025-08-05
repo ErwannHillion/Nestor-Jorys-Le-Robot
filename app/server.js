a72321cf90509874cf92327d5bf99ef8aaa03e5d
Erwann <erwannhillion01@gmail.com>
2025-08-05 10:48:35 +0200

    feat(action): adding action url with get method

---
+++ app/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/action', (req, res) => {
+    const moves = ["UP", "DOWN", "LEFT", "RIGHT", "STAY"];
+
+    const randomMove = moves[Math.floor(Math.random() * moves.length)];
+
+    res.json({
+        "move": randomMove
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
