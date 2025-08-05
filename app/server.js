e06fec7466070e3efbe8b6b1d137c69ceaacfbd0
Erwann <erwannhillion01@gmail.com>
2025-08-05 12:38:37 +0200

    fix(move): adding manual move and action

--- app/server.js
+++ app/server.js
@@ -4,21 +4,32 @@ const cors = require('cors');
 const port = 3000;
 
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+
+let currentMove = "STAY";
+let currentAction = "NONE";
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/action', (req, res) => {
-    const moves = ["UP", "DOWN", "LEFT", "RIGHT", "STAY"];
-    const actions = ["COLLECT", "BOMB", "NONE"];
-
-    const randomMove = moves[Math.floor(Math.random() * moves.length)];
-    const randomAction = actions[Math.floor(Math.random() * actions.length)];
     res.json({
-        "move": randomMove,
-        "action": randomAction
+        "move": currentMove,
+        "action": currentAction
     });
+});
+
+app.post('/set-move', (req, res) => {
+    const { move } = req.body;
+    currentMove = move;
+    res.redirect('/');
+});
+
+app.post('/set-action', (req, res) => {
+    const { action } = req.body;
+    currentAction = action;
+    res.redirect('/');
 });
 
 app.listen(port, () => {
